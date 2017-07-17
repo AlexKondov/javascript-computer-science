4ffd4356f4522f41084ef7dc6623b03a74e5f18d
Alex Kondov <alexkondov38@gmail.com>
2017-07-18 01:00:26 +0300

    committing second merge sort version

--- mergeSortNoCopying.js
+++ mergeSortNoCopying.js
@@ -1,35 +1,37 @@
 // Split the array into halves and merge them recursively
-function mergeSort (arr) {
-  if (arr.length === 1) {
+function mergeSort (arr, start = 0, end = arr.length - 1) {
+  var middle = Math.floor((start + end) / 2)
+
+  if (start === end) {
     // return once we hit an array with a single item
-    return arr
+    return [arr[start]]
   }
 
-  const middle = Math.floor(arr.length / 2) // get the middle item of the array rounded down
-  const left = arr.slice(0, middle) // items on the left side
-  const right = arr.slice(middle) // items on the right side
-
   return merge(
-    mergeSort(left),
-    mergeSort(right)
+    mergeSort(arr, start, middle),
+    mergeSort(arr, middle + 1, end)
   )
 }
 
 // compare the arrays item by item and return the concatenated result
 function merge (left, right) {
+  console.log(left, right)
   let result = []
   let indexLeft = 0
   let indexRight = 0
 
   while (indexLeft < left.length && indexRight < right.length) {
     if (left[indexLeft] < right[indexRight]) {
-      result.push(left[indexLeft++])
+      result.push(left[indexLeft])
+      left.splice(indexLeft, 1)
     } else {
-      result.push(right[indexRight++])
+      result.push(right[indexRight])
+      right.splice(indexRight, 1)
     }
   }
 
-  return result.concat(left.slice(indexLeft)).concat(right.slice(indexRight))
+  result = result.concat(left).concat(right)
+  return result
 }
 
 const list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
